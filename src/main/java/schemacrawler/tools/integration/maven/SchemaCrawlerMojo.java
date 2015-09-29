d253de6dcdbf1f16bb89cbe6ae2d11bf1319c06a
Sualeh Fatehi <sualeh@hotmail.com>
2015-09-28 21:43:44 -0400

    Fixing annotations, and build plugins

--- src/main/java/schemacrawler/tools/integration/maven/SchemaCrawlerMojo.java
+++ src/main/java/schemacrawler/tools/integration/maven/SchemaCrawlerMojo.java
@@ -1,23 +1,21 @@
 /*
- * #%L
- * SchemaCrawler Maven Plugin
- * %%
- * Copyright (C) 2011 SchemaCrawler
- * %%
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as 
- * published by the Free Software Foundation, either version 3 of the 
- * License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Lesser Public License for more details.
- * 
- * You should have received a copy of the GNU General Lesser Public 
- * License along with this program.  If not, see
- * <http://www.gnu.org/licenses/lgpl-3.0.html>.
- * #L%
+ *
+ * SchemaCrawler Report - Maven Plugin
+ * http://www.schemacrawler.com
+ * Copyright (c) 2011-2015, Sualeh Fatehi.
+ *
+ * This library is free software; you can redistribute it and/or modify it under the terms
+ * of the GNU Lesser General Public License as published by the Free Software Foundation;
+ * either version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with this
+ * library; if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
  */
 package schemacrawler.tools.integration.maven;
 
@@ -44,7 +42,6 @@ import org.apache.maven.artifact.Artifact;
 import org.apache.maven.doxia.sink.Sink;
 import org.apache.maven.doxia.siterenderer.Renderer;
 import org.apache.maven.plugin.logging.Log;
-import org.apache.maven.plugins.annotations.Component;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
@@ -77,7 +74,7 @@ public class SchemaCrawlerMojo
   private static final String INCLUDE_ALL = ".*";
   private static final String INCLUDE_NONE = "";
 
-  @Component
+  @Parameter(defaultValue = "${project}", readonly = true)
   private MavenProject project;
 
   /**
